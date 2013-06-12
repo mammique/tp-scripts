651271300a095d5c24b3214123d8c18b6129bb73
Camille Harang <mammique@yooook.net>
2013-06-13 01:08:46 +0200

    first commit

---
+++ c74eca22-d3b3-11e2-b49f-00163e84330e.php
@@ -0,0 +1,215 @@
+<?php
+
+$tp_jocelyne      = 'f09d35de-ce28-11e2-94a7-00163e84330e';
+$tp_seance        = '022f708c-ce29-11e2-94a7-00163e84330e';
+$tp_meta_donation = 'fa2a36d0-ce8f-11e2-bfd8-00163e84330e';
+$tp_meta_vod_fr   = 'c0e9d4a2-ce8f-11e2-bfd8-00163e84330e';
+$tp_meta_vod_en   = '2de22902-ce99-11e2-94a7-00163e84330e';
+
+if($step == 'complete') {
+
+    $price          = (float)$pp_result['mc_gross'];
+    $partner_share  = $price*0.075;
+    $jocelyne_share = $price - ($partner_share * 2) - (float)$pp_result['mc_fee'];
+
+    $tp_meta = null;
+
+    switch($query['tp_item']) {
+    
+        case 'movie_fr':
+
+            $tp_meta = $tp_meta_vod_fr;
+            break;
+    
+        case 'movie_en':
+
+            $tp_meta = $tp_meta_vod_en;
+            break;
+    
+        case 'donation':
+
+            $tp_meta = $tp_meta_donation;
+            break;
+    }
+
+    if($tp_meta)
+        $tp_client->post("metadata/snippet/$tp_meta/update/assigned_quantities/add/")
+            ->addPostFields(array('uuid' => $quantity['uuid']))->send();
+
+    $request = $tp_client->post('value/quantity/create/')
+                   ->addPostFields(array('unit'            => $tp_unit,
+                                         'quantity'        => $partner_share,
+                                         'user'            => $tp_auth_user,
+                                         'user_visibility' => 'public',
+                                         'prev'            => $quantity['uuid'],
+                                         'status'          => 'present'));
+    $tp_share = $request->send()->json();
+
+    $request = $tp_client->post('value/quantity/create/')
+                   ->addPostFields(array('unit'            => $tp_unit,
+                                         'quantity'        => $partner_share,
+                                         'user'            => $tp_auth_user,
+                                         'user_visibility' => 'public',
+                                         'prev'            => $quantity['uuid'],
+                                         'status'          => 'present'));
+    $tp_seance_share = $request->send()->json();
+
+    $request = $tp_client->post('value/quantity/create/')
+                   ->addPostFields(array('unit'            => $tp_unit,
+                                         'quantity'        => $partner_share,
+                                         'user'            => $tp_seance,
+                                         'user_visibility' => 'public',
+                                         'prev'            => $tp_seance_share['uuid'],
+                                         'status'          => 'pending'));
+    $seance_share = $request->send()->json();
+
+    $request = $tp_client->post('value/quantity/create/')
+                   ->addPostFields(array('unit'            => $tp_unit,
+                                         'quantity'        => $jocelyne_share,
+                                         'user'            => $tp_auth_user,
+                                         'user_visibility' => 'public',
+                                         'prev'            => $quantity['uuid'],
+                                         'status'          => 'present'));
+    $tp_jocelyne_share = $request->send()->json();
+
+    $request = $tp_client->post('value/quantity/create/')
+                   ->addPostFields(array('unit'            => $tp_unit,
+                                         'quantity'        => $jocelyne_share,
+                                         'user'            => $tp_jocelyne,
+                                         'user_visibility' => 'public',
+                                         'prev'            => $tp_jocelyne_share['uuid'],
+                                         'status'          => 'pending'));
+    $jocelyne_share = $request->send()->json();
+
+    if($query['tp_item'] == 'donation') {
+
+        echo '<p>Merci pour votre don !</p>';
+
+        if(!array_key_exists('anon', $query)) {
+
+            $quantity['user_visibility'] = 'public';
+            $request = $tp_client->put($quantity['url'].'update/')
+                ->setBody(json_encode($quantity), 'application/json');
+            $request->send();
+        }
+
+        if(array_key_exists('mess', $query)) {
+
+            $request = $tp_client->post('metadata/snippet/create/')
+                           ->addPostFields(array('user'                => $tp_auth_user,
+                                                 'visibility'          => 'public',
+                                                 'mimetype'            => 'text/plain',
+                                                 'slug'                => "liberator_comment",
+                                                 'type'                => 'comment',
+                                                 'assigned_quantities' => $quantity['uuid'],
+                                                 'content'             => $query['mess']));
+            $metadata = $request->send()->json();
+        }
+
+    } elseif(($query['tp_item'] == 'movie_fr' || $query['tp_item'] == 'movie_en') && $price < 6) {
+
+        echo '<p class="error">Invalid amount.</p>';
+        exit();
+    }
+
+    if($tp_meta) {
+
+        $request  = $tp_client->get("metadata/snippet/$tp_meta/content");
+        $response = $request->send();
+        $tp_meta  = $response->json();
+
+        if($query['tp_item'] == 'movie_en')
+
+            $body = "Hello ".$user['name'].", ".
+                "thank you for your participation to the liberation of the movie ".
+                "« Me, Finance and Sustainable Development » with:\n\n";
+
+        else
+
+            $body = "Bonjour ".$user['name'].", ".
+                "merci de votre participation à la libération du film ".
+                "« Moi, la Finance et le Développement Durable » avec :\n\n";
+
+        $body .= $tp_meta['desc']."\n\n";
+
+        if($query['tp_item'] == 'movie_en')
+
+            $body .= "When you'll want to access the movie again later on, you might have to reconnect via Traceparent. ".
+                "In order to set or reset your Traceparent password, you'll need to visit this page (and provide this very same ".
+                "email: ".$pp_result['payer_email']."): ".
+                "$tp_url/extra/ui/#/auth/password_reset/\n\n";
+
+        elseif($query['tp_item'] == 'movie_fr')
+
+            $body .= "Quand vous voudrez de nouveau accéder au film à l'avenir, vous aurez peut-être besoin de vous reconnecter via Traceparent. ".
+                "Pour (ré)initialiser votre mot-de-passe Traceparent, vous aurez besoin de visiter cette page (et y entrer ce même ".
+                "email : ".$pp_result['payer_email']."): ".
+                "$tp_url/extra/ui/#/auth/password_reset/\n\n";
+
+        if($query['tp_item'] == 'movie_en')
+
+            $body .= "You can check information about your payment on your PayPal account at https://paypal.com/".
+                "\n\n".
+                "Tracability and transparency about the use of your money is monitored ".
+                "by the free and Open Source software Traceparent (http://traceparent.com/) ".
+                "that is running the funding gauge on our website.".
+                "\n\n".
+                "Best regards,".
+                "\n\nJocelineaste.\n\nhttp://financedurable-lefilm.com/";
+        else
+
+            $body .= "Vous pouvez vérifier les information de votre paiement sur votre compte ".
+                "PayPal à l'adresse https://paypal.com/".
+                "\n\n".
+                "La traçabilité et la transparence sur la utilisation de votre argent ".
+                "est suivie par le Logiciel Libre et Open Source Traceparent (http://".
+                "traceparent.com/) qui opère le fonctionnement de la jauge de financement ".
+                "présente sur notre site.".
+                "\n\n".
+                "Cordialement,".
+                "\n\nJocelineaste.\n\nhttp://financedurable-lefilm.com/";
+
+
+        mail($pp_result['payer_email'],
+             '[Jocelineaste] '.$tp_meta['label'], $body,
+             'From: '.$admin_mail."\r\n"."Content-Type: text/plain; charset=UTF-8\r\n");
+
+        if($query['tp_item'] == 'movie_fr' || $query['tp_item'] == 'movie_en') {
+
+            if($query['tp_item'] == 'movie_fr') echo '<p>Commande réussie.</p>';
+            else echo '<p>Order successful.</p>';
+
+            $tp_client->post("extra/serve/bucket/$tp_jocelyne/financedurable/update/users_ro/add/")
+                ->addPostFields(array('uuid' => $user['uuid']))->send();
+        }
+    }
+}
+
+if($query['tp_item'] == 'donation') {
+
+    echo '<script type="text/javascript">document.location="http://financedurable-lefilm.com/";</script>';
+    exit();
+}
+
+if($step == 'transaction_already_registered') {
+
+    $base_url = $tp_url.'/extra/ui/#/auth/login/';
+    $amp_mark = '?';
+
+} else {
+    exec('PYTHONPATH='.$tp_virtualenv.'/lib/python2.7/site-packages/:'.
+         $tp_virtualenv.'/src/traceparent/ '.
+         $tp_virtualenv.'/src/traceparent/scripts/auth_user_token.py '.$pp_result['payer_email'],
+         $output, $return_var);
+    $base_url = $tp_url.'/auth/login/?login_token='.$output[0];
+    $amp_mark = '&';
+}
+
+
+if($query['tp_item'] == 'movie_fr') $redirect_url = $base_url.$amp_mark.'next=http%3A//financedurable-lefilm.com/Voir-le-film-en-ligne';
+else $redirect_url = $base_url.$amp_mark.'next=http%3A//financedurable-lefilm.com/Watch-online';
+
+echo '<script type="text/javascript">document.location="'.$redirect_url.'";</script>';
+exit();
+
+?>
---
+++ paypal_pdt.php
@@ -0,0 +1,201 @@
+<?php
+
+require('paypal_pdt.config.php');
+
+/**
+ * http://www.paulund.co.uk/parse-url-querystring-into-array-in-php
+ * 
+ * Parse out url query string into an associative array
+ *
+ * $qry can be any valid url or just the query string portion.
+ * Will return false if no valid querystring found
+ *
+ * @param $qry String
+ * @return Array
+ */
+function query_dict($qry) {
+    $result = array();
+    //string must contain at least one = and cannot be in first position
+    if(strpos($qry,'=')) {
+     if(strpos($qry,'?')!==false) {
+       $q = parse_url($qry);
+       $qry = $q['query'];
+      }
+    }else {
+            return false;
+    }
+    foreach (explode('&', $qry) as $couple) {
+            list ($key, $val) = explode('=', $couple);
+            $result[$key] = rawurldecode($val);
+    }
+    return empty($result) ? false : $result;
+}
+
+$step = null;
+
+if(!array_key_exists('cm', $_GET)) exit();
+$query = query_dict(rawurldecode($_GET['cm']));
+
+if(!array_key_exists('tp_scope', $query)) exit();
+$tp_scope = $query['tp_scope'];
+
+preg_match("/[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}/", $tp_scope, $matches);
+if(!$matches) exit();
+
+require_once 'vendor/autoload.php';
+
+$pp_result = array();
+$req = 'cmd=_notify-synch';
+$tx_token = $_GET['tx'];
+$req .= "&tx=$tx_token&at=$pp_auth_token";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "$pp_url/cgi-bin/webscr");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
+curl_setopt($ch, CURLOPT_CAINFO, __DIR__.'/cacert.pem');
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+//curl_setopt($ch, CURLOPT_HTTPHEADER, array("Host: $pp_url"));
+//curl_setopt($ch, CURLOPT_HTTPHEADER, array("Host: www.sandbox.paypal.com"));
+$res = curl_exec($ch);
+
+$error_el = '<p class="error">'.
+            "Something went wrong, our staff is working on it, sorry for the inconvenience.".
+            '</p>';
+
+if(!$res) {
+
+    echo $error_el;
+    mail($admin_mail,
+         '[Traceparent Scripts] Error',
+         'curl_error: '.curl_error($ch)."\n\nreq: ".$req."\n\nres: ".$res, 'From: '.$admin_mail."\r\n");    
+    curl_close($ch);
+    exit();
+}
+
+curl_close($ch);
+$lines = explode("\n", $res);
+
+if (strcmp($lines[0], "SUCCESS") != 0) {
+
+    echo $error_el;
+    mail($admin_mail,
+         '[Traceparent Scripts] Error',
+         "req: ".$req."\n\nres: ".$res, 'From: '.$admin_mail."\r\n");
+    exit();
+}
+
+for ($i = 1 ; $i < count($lines) ; $i++){
+
+    if(strlen($lines[$i])) {
+
+        list($key, $val) = explode("=", $lines[$i]);
+        $pp_result[urldecode($key)] = urldecode($val);
+    }
+}
+
+if($pp_result['receiver_email'] != $pp_email) {
+
+    echo '<p class="error">Receiver email address mismatch.</p>';
+    exit();
+}
+
+global $tp_client;
+$tp_client = new Guzzle\Http\Client($tp_url,
+                 array('curl.options' => array(
+                    CURLOPT_HTTPHEADER => array(
+                        "Authorization: Token $tp_auth_token"))));
+
+$pp_id = $pp_result['txn_id'];
+
+$request = $tp_client->get("value/unit/$tp_unit/");
+$unit = $request->send()->json();
+
+if($pp_result['mc_currency'] != $unit['slug']) {
+
+    echo '<p class="error">'.
+         "Currency mismatch.".
+         '</p>';
+    exit();
+}
+
+$request = $tp_client->get('metadata/snippet/filter/');
+$request->getQuery()->set('slug_0', "paypal_pdt_$pp_id");
+$request->getQuery()->set('slug_1', 'exact');
+$request->getQuery()->set('page_size', '0');
+$metadata_prev = $request->send()->json();
+
+if(count($metadata_prev)) {
+
+    echo '<p class="error">'.
+         "Transaction already registered.".
+         '</p>';
+    $step = 'transaction_already_registered';
+    require($tp_scope.'.php');
+    exit();
+}
+
+function get_or_create_user($email, $name='', $details=false) {
+
+    global $tp_client;
+
+    $request = $tp_client->get('auth/user/filter/');
+    $request->getQuery()->set('email', $email);
+    $request->getQuery()->set('page_size', 1);
+
+    $response = $request->send();
+    $data = $response->json();
+
+    if(count($data['results'])) {
+
+        if(!$details) return $data['results'][0];
+        else return $tp_client->get('auth/user/'.$data['results'][0]['uuid'].'/')->send()->json();
+    }
+
+    $request = $tp_client->post('auth/user/create/')
+                   ->addPostFields(array('email'    => $email,
+                                         'name'     => $name,
+                                         'password' => ''));
+    return $request->send()->json();
+}
+
+$user = get_or_create_user($pp_result['payer_email'], $pp_result['first_name'].' '.$pp_result['last_name']);
+
+$request = $tp_client->post('value/quantity/create/')
+               ->addPostFields(array('unit'            => $tp_unit,
+                                     'quantity'        => $pp_result['mc_gross'],
+                                     'user'            => $user['uuid'],
+                                     'user_visibility' => 'private',
+                                     'status'          => 'passed'));
+$quantity = $request->send()->json();
+
+$request = $tp_client->post('metadata/snippet/create/')
+               ->addPostFields(array('user'                => $tp_auth_user,
+                                     'visibility'          => 'private',
+                                     'mimetype'            => 'application/json',
+                                     'slug'                => "paypal_pdt_$pp_id",
+                                     'type'                => 'paypal_pdt',
+                                     'assigned_quantities' => $quantity['uuid'],
+                                     'content'             => json_encode($pp_result)));
+$metadata = $request->send()->json();
+
+$request = $tp_client->post("monitor/scope/$tp_scope/update/quantities/add/")
+               ->addPostFields(array('uuid' => $quantity['uuid']))->send();
+
+if(!array_key_exists('mc_fee', $pp_result)) $pp_result['mc_fee'] = 0;
+
+$request = $tp_client->post('value/quantity/create/')
+               ->addPostFields(array('unit'            => $tp_unit,
+                                     'quantity'        => $pp_result['mc_fee'],
+                                     'user'            => $pp_tp_uuid,
+                                     'user_visibility' => 'public',
+                                     'prev'            => $quantity['uuid'],
+                                     'status'          => 'present'));
+$pp_quantity = $request->send()->json();
+
+$step = 'complete';
+require($tp_scope.'.php');
+
+?>
